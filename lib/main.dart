da865dc79ff1a00bdcb3bb84b19c1076528a46df
ayumisyahirah <email-ayumipcy23@gmail.com>
2024-12-20 23:50:47 +0700

    Main

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,215 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'kalender_page.dart';
+import 'todo_list.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:timezone/data/latest.dart' as tz;
+import 'package:timezone/timezone.dart' as tz;
+import 'package:permission_handler/permission_handler.dart';
 
-void main() {
-  runApp(const MyApp());
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final FlutterLocalNotificationsPlugin notificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+
+  const AndroidInitializationSettings androidInitSettings =
+      AndroidInitializationSettings('@mipmap/ic_launcher');
+  const InitializationSettings initSettings =
+      InitializationSettings(android: androidInitSettings);
+
+  await notificationsPlugin.initialize(initSettings);
+
+  tz.initializeTimeZones();
+  runApp(MyApp(notificationsPlugin: notificationsPlugin));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  final FlutterLocalNotificationsPlugin notificationsPlugin;
+  const MyApp({super.key, required this.notificationsPlugin});
 
-  // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  ThemeMode _themeMode = ThemeMode.light;
+  final FlutterLocalNotificationsPlugin _notificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+
+  @override
+  void initState() {
+    super.initState();
+    _checkNotificationPermission();
+    _checkExactAlarmPermission();
+    _initializeApp();
+    _initializeNotifications();
+    _loadThemePreference();
+  }
+
+  Future<void> _initializeApp() async {
+    if (Platform.isAndroid && Platform.version.contains("12")) {
+      final permissionStatus = await Permission.scheduleExactAlarm.status;
+      if (permissionStatus.isDenied) {
+        _showPermissionDialog(
+          "Izin Exact Alarm Dibutuhkan",
+          "Aplikasi memerlukan izin Exact Alarm agar fitur dapat berjalan dengan baik.",
+        ); // Tampilkan dialog izin
+      }
+    }
+  }
+
+  Future<void> _checkExactAlarmPermission() async {
+    print("Memeriksa izin Exact Alarm...");
+    if (Platform.isAndroid && Platform.version.contains("12")) {
+      final permissionStatus = await Permission.scheduleExactAlarm.status;
+
+      print("Status izin Exact Alarm: $permissionStatus");
+
+      if (permissionStatus.isDenied || permissionStatus.isPermanentlyDenied) {
+        print("Izin Exact Alarm ditolak.");
+        // Tampilkan dialog untuk meminta pengguna membuka pengaturan
+        _showPermissionDialog("Izin Exact Alarm",
+            "Aplikasi memerlukan izin Exact Alarm untuk menjadwalkan notifikasi.");
+      } else {
+        print("Exact Alarm tidak diperlukan di platform ini.");
+      }
+    } else {
+      print("Exact Alarm tidak diperlukan di platform ini.");
+    }
+  }
+
+  void _showPermissionDialog(String title, String message) {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text(title),
+        content: Text(message),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.of(context).pop(),
+            child: const Text("Batal"),
+          ),
+          TextButton(
+            onPressed: () async {
+              Navigator.of(context).pop();
+              await openAppSettings(); // Buka pengaturan aplikasi
+            },
+            child: const Text("Pengaturan"),
+          ),
+        ],
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
-}
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  Future<void> _checkNotificationPermission() async {
+    final status = await Permission.notification.status;
+    if (status.isDenied) {
+      final result = await Permission.notification.request();
+      if (result.isGranted) {
+        print("Izin notifikasi diberikan.");
+      } else {
+        print("Izin notifikasi ditolak.");
+      }
+    } else if (status.isPermanentlyDenied) {
+      _showPermissionDialog(
+        "Izin Notifikasi Dibutuhkan",
+        "Aplikasi memerlukan izin notifikasi agar fitur dapat berfungsi dengan baik.",
+      );
+    }
+  }
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+  Future<void> _initializeNotifications() async {
+    const AndroidInitializationSettings androidInitializationSettings =
+        AndroidInitializationSettings('@mipmap/ic_launcher');
 
-  final String title;
+    const InitializationSettings initializationSettings =
+        InitializationSettings(android: androidInitializationSettings);
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
+    final bool? initialized =
+        await _notificationsPlugin.initialize(initializationSettings);
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+    print("Notification plugin initialized: $initialized");
+    tz.initializeTimeZones();
+  }
 
-  void _incrementCounter() {
+  Future<void> _showNotification(
+      String title, String body, DateTime dateTime) async {
+    try {
+      await _notificationsPlugin.zonedSchedule(
+        0,
+        title,
+        body,
+        tz.TZDateTime.from(dateTime, tz.local),
+        const NotificationDetails(
+          android: AndroidNotificationDetails(
+            'channel_id',
+            'channel_name',
+            channelDescription: 'Notification channel for calendar app',
+            importance: Importance.high,
+            priority: Priority.high,
+          ),
+        ),
+        uiLocalNotificationDateInterpretation:
+            UILocalNotificationDateInterpretation.absoluteTime,
+        androidScheduleMode: AndroidScheduleMode.inexactAllowWhileIdle,
+      );
+    } catch (e) {
+      print("Error scheduling notification: $e");
+    }
+  }
+
+
+  Future<void> _loadThemePreference() async {
+    final prefs = await SharedPreferences.getInstance();
+    final isDarkMode = prefs.getBool('isDarkMode') ?? false;
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _themeMode = isDarkMode ? ThemeMode.dark : ThemeMode.light;
     });
   }
 
+  Future<void> _toggleTheme() async {
+    final prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _themeMode =
+          _themeMode == ThemeMode.light ? ThemeMode.dark : ThemeMode.light;
+    });
+    prefs.setBool('isDarkMode', _themeMode == ThemeMode.dark);
+  }
+
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Kalender Event',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        brightness: Brightness.light,
+        appBarTheme: const AppBarTheme(
+          // backgroundColor: Colors.white, // Warna untuk light mode
+          foregroundColor: Colors.black, // Teks dan ikon untuk light mode
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      darkTheme: ThemeData(
+        primarySwatch: Colors.blue,
+        brightness: Brightness.dark,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.black, // Warna untuk dark mode
+          foregroundColor: Colors.white, // Teks dan ikon untuk dark mode
+        ),
+      ),
+      themeMode: _themeMode,
+      home: KalenderPage(onToggleTheme: _toggleTheme, notificationsPlugin: widget.notificationsPlugin),
+      routes: {
+        '/todo': (context) => ToDoListPage(
+          notificationsPlugin: _notificationsPlugin,
+        ),
+      },
     );
   }
 }
